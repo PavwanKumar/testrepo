be06d065ad7eb1f1d0b577cf7923934acbfabaea
Pavwan <pavwankumar08@gmail.com>
2021-12-23 19:35:52 +0530

    FirstProgram Commit

---
+++ FirstProgram.java
@@ -0,0 +1,15 @@
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+	
+		System.out.println("This is my First Program - Test");
+		
+		for(int i=0; i<10; i++)
+		{
+			System.out.println("Loop Test " +i);
+		}
+
+	}
+
+}
